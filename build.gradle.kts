7439b23c51a997808c232c37936e7148b0305e73
aiwriter447-web <aiwriter447@gmail.com>
2026-09-20 21:47:07 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
-org.gradle.jvmargs=-Xmx4096m -Dfile.encoding=UTF-8 -XX:+UseParallelGC
-org.gradle.parallel=false
-org.gradle.caching=true
-org.gradle.configuration-cache=false
-
-android.useAndroidX=true
-android.nonTransitiveRClass=true
-
-kotlin.code.style=official
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
