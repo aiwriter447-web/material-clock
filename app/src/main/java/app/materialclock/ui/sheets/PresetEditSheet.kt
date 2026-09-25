f0b9d5587152fc896c513e7883fd49c4ffe6ccca
aiwriter447-web <aiwriter447@gmail.com>
2026-09-26 00:16:58 +0530

    Update PresetEditSheet.kt

--- app/src/main/java/app/materialclock/ui/sheets/PresetEditSheet.kt
+++ app/src/main/java/app/materialclock/ui/sheets/PresetEditSheet.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.Icons.Rounded.Delete
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
